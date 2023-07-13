e54fec2c19a4edeeae9a1bdb093634c2bcfa2763
nacir1i <taoufiqnaciri234@gmail.com>
2023-07-13 12:15:06 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.postcss";
+  import { DarkMode } from "flowbite-svelte";
+</script>
+
+<body class="dark:bg-gray-800 w-screen h-screen">
+  <DarkMode />
+  <slot />
+</body>
