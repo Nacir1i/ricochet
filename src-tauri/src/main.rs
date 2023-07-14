babe461b11e109023a4f06128df4ca8995b035cd
nacir1i <taoufiqnaciri234@gmail.com>
2023-07-14 19:53:51 +0100

    full setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    use tauri::Manager;
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
