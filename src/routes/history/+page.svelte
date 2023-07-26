53e8eece1e070c194ed2b13ef57c4da833c54455
nacir1i <taoufiqnaciri234@gmail.com>
2023-07-26 01:58:05 +0100

    update history page

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -3,7 +3,9 @@
   import Game from "$lib/game.svelte";
 </script>
 
-<div class="w-full h-full p-3 flex flex-wrap gap-3 content-start">
+<div
+  class="w-full h-full p-3 flex flex-wrap gap-3 content-start overflow-scroll"
+>
   {#each $history as game}
     <Game {game} />
   {/each}
