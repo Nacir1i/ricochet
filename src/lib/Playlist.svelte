a8cdaab7f44c49bcb1d55c6388107199d19cc5b4
nacir1i <taoufiqnaciri234@gmail.com>
2023-08-27 05:08:24 +0100

    update playlist component

--- src/lib/Playlist.svelte
+++ src/lib/Playlist.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import type { GroupedPlaylist } from "$lib/util";
+  import { invoke } from "@tauri-apps/api";
+  import { type GroupedPlaylist, PlaylistState } from "$lib/util";
+  import { Button } from "flowbite-svelte";
   import { ChevronDown, ChevronUp } from "lucide-svelte";
 
   export let playlist: GroupedPlaylist;
@@ -7,35 +9,78 @@
   let toggle = false;
 </script>
 
-<div class="bg-red-300 p-3">
-  <p>name: {playlist.name}</p>
-  <p>description: {playlist.description}</p>
-  <p>duration: {playlist.duration}</p>
-  <p>duration: {playlist.state}</p>
-  <button
-    on:click={() => {
-      toggle = !toggle;
-      console.log("clicked");
-    }}
-  >
-    {#if toggle}
-      <ChevronUp class="cursor-pointer" />
-    {:else}
-      <ChevronDown class="cursor-pointer" />
-    {/if}
-  </button>
+<div
+  data-te-perfect-scrollbar-init
+  class="w-[500px] max-w-[500px] dark:bg-gray-700 bg-gray-200 p-3 dark:text-white rounded-lg"
+>
+  <div class="flex items-center justify-between mb-2">
+    <div class="flex flex-col gap-2">
+      <span class="flex items-center gap-2 mb-1">
+        <p class="text-lg">Name:</p>
+        <p>{playlist.name}</p>
+        <Button
+          class="py-1 px-2"
+          color={playlist.state === PlaylistState.ACTIVE ? "green" : "red"}
+          on:click={() =>
+            invoke("update_playlist_state", {
+              playlistId: playlist.id,
+              state:
+                playlist.state === PlaylistState.ACTIVE
+                  ? PlaylistState.INACTIVE
+                  : PlaylistState.ACTIVE,
+            })}>{playlist.state}</Button
+        >
+      </span>
+      <span class="flex items-center gap-2">
+        <p class="text-lg">Description:</p>
+        <p>{playlist.description}</p>
+      </span>
+      <span class="flex items-center gap-2">
+        <p class="text-lg">Duration:</p>
+        <p>{playlist.duration}</p>
+      </span>
+    </div>
+
+    <button
+      on:click={() => {
+        toggle = !toggle;
+        console.log("clicked");
+      }}
+    >
+      {#if toggle}
+        <ChevronUp class="cursor-pointer" />
+      {:else}
+        <ChevronDown class="cursor-pointer" />
+      {/if}
+    </button>
+  </div>
   {#if toggle}
-    <div class="bg-green-300">
+    <div
+      class="border-t-2 dark:border-white border-gray-400 pt-2 flex flex-col gap-5"
+    >
       {#each playlist.scenarios as scenario}
-        <p>{scenario.scenario_name}</p>
-        <p>{scenario.scenario_difficulty}</p>
-        <p>{scenario.reps}</p>
-        Days:
-        <ul class="list-disc ml-3 p-5">
-          {#each scenario.days as day}
-            <li>{day.games_count}</li>
-          {/each}
-        </ul>
+        <div>
+          <span class="flex items-center gap-2">
+            <p>Scenario name :</p>
+            <p>{scenario.scenario_name}</p>
+          </span>
+          <span class="flex items-center gap-2">
+            <p>Scenario difficulty :</p>
+            <p>{scenario.scenario_difficulty}</p>
+          </span>
+          <span class="flex items-center gap-2">
+            <p>Repititions :</p>
+            <p>{scenario.reps}</p>
+          </span>
+          <span>
+            <p>Average accuracy per day :</p>
+            <ul class="list-decimal ml-3 pl-5">
+              {#each scenario.days as day}
+                <li>{day.games_count}</li>
+              {/each}
+            </ul>
+          </span>
+        </div>
       {/each}
     </div>
   {/if}
